f74ebc59449d76963e3e393c17da79b41a1cbca4
Jesper <jesper@jesper.tk>
2023-03-06 17:26:03 +0100

    1.0.6 FIX 2 ende

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/ht-hosting", function (req, res) {
-  res.redirect("https://cp.ht-hosting.de/aff.php?aff=169");
-});
-
-app.listen(3000);
