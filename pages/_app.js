fbd362b08fdd2156b33d61fedb06ccbc0674311c
Jesper <contact@tnsjesper.xyz>
2023-07-07 15:16:40 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
+import '/css/tailwind.css'
+import '/css/prism.css'
 import 'katex/dist/katex.css'
 
 import '@fontsource/inter/variable-full.css'
@@ -7,10 +7,10 @@ import '@fontsource/inter/variable-full.css'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
+import siteMetadata from '/data/siteMetadata'
+import Analytics from '/components/analytics'
+import LayoutWrapper from '/components/LayoutWrapper'
+import { ClientReload } from '/components/ClientReload'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
