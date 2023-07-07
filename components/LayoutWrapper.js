9c4f64f1bf66a2a408d5613493ae2ff6d1799125
Jesper <contact@tnsjesper.xyz>
2023-07-07 15:23:53 +0200

    Update Footer and LayoutWarper

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
-import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import siteMetadata from '/data/siteMetadata'
+import headerNavLinks from '/data/headerNavLinks'
+
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -15,9 +15,6 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                {/* <div className="mr-3">
-                  <Logo />
-                </div> */}
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
